22130fd3b0ebc4675d9f694b2c0ff9ab854c2d16
ritz102 <101417810+ritz102@users.noreply.github.com>
2023-06-18 01:08:03 +0800

    Add files via upload

---
+++ Basic_commands.py
@@ -0,0 +1,23 @@
+
+from djitellopy import Tello # ensure library is installed from settings prior to testing
+import time
+
+tello = Tello() # ensure drone is connected to the laptop
+
+tello.connect() # Connects the tello
+tello.takeoff() # Sends the command to the tello drone to takeoff
+tello.move_up(80) # Sends the command to move the tello upwards 80cm
+
+tello.move_down(50)  # Sends the command to move the tello 50cm downwards
+
+tello.move_left(50)  # Sends the command to move the tello  50cm to the left
+
+tello.move_right(50) # Sends the command to move the tello  50cm to the right
+
+tello.land() # sends a landing command to the tello
+
+tello.disconnect() # Disconnects the tello to the code
+
+time.sleep(2)
+
+# Distances in parantheses are given in centimeteres !
