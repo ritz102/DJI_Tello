13acdfedb04a9b4db254f57d90bd602707ccffbe
ritz102 <101417810+ritz102@users.noreply.github.com>
2023-06-18 01:08:57 +0800

    Basic_commands.py

--- Basic_commands.py
+++ Basic_commands.py
@@ -1,4 +1,4 @@
-
+3e
 from djitellopy import Tello # ensure library is installed from settings prior to testing
 import time
 
@@ -18,6 +18,6 @@ tello.land() # sends a landing command to the tello
 
 tello.disconnect() # Disconnects the tello to the code
 
-time.sleep(2)
+time.sleep(2) #given in seconds 
 
-# Distances in parantheses are given in centimeteres !
+# Distances in parentheses are given in centimeters!
