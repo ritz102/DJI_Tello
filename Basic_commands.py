c075813a6d45839d9a922655fadef3dcbf1dc4cd
ritz102 <101417810+ritz102@users.noreply.github.com>
2023-06-22 09:29:54 +0800

    Update Basic_commands.py

--- Basic_commands.py
+++ Basic_commands.py
@@ -2,7 +2,7 @@
 from djitellopy import Tello # ensure library is installed from settings prior to testing
 import time
 
-
+drone = Tello()
 tello.connect() # Connects the tello
 tello.takeoff() # Sends the command to the tello drone to takeoff
 tello.move_up(80) # Sends the command to move the tello upwards 80cm
