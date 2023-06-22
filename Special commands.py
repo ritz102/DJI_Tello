f0d81e1660ca1e63f1369cd351840107547d04b1
ritz102 <101417810+ritz102@users.noreply.github.com>
2023-06-22 16:18:57 +0800

    Special commands.py

--- Special commands.py
+++ Special commands.py
@@ -7,9 +7,16 @@ drone.takeoff() # Gives the command to take off the drone
 drone.move_forward(70) # Commands the drone to move forward by 70cm
 drone.move_up(50) # Gives the command to move the drone up by 50cm
 
+
+# Commands to execute flips using the Tello Drone 
 drone.flip("f")  # Gives the command for the drone to do a forward flip
-drone.flip_left() # Gives the command for the drone to do a left sided flip
-drone.flip_right() # Gives the commmand for the drone to do a right sided flip
-drone.flip_back()  # Gives the command for the drone to a back flip
+drone.flip_left() # Gives the command for the drone to do a left-sided flip
+drone.flip_right() # Gives the command for the drone to do a right-sided flip
+drone.flip_back()  # Gives the command for the drone to a backflip
+
+# Commands to execute flips using the Tello Drone 
+drone.rotate_cw(120) # Gives the command for the drone to rotate 120 degrees in a clockwise direction.
+drone.rotate_counter_clockwise(90) #  # Gives the command for the drone to rotate 120 degrees in a counter-clockwise direction.
+
 
 drone.land()
