7a6346a58f9c33bea72d8a32f1cd4cd07e3b48b0
ritz102 <101417810+ritz102@users.noreply.github.com>
2023-08-28 11:14:53 +0800

    Basic_commands.py

--- Basic_commands.py
+++ Basic_commands.py
@@ -3,19 +3,19 @@ from djitellopy import Tello # ensure library is installed from settings prior t
 import time
 
 drone = Tello()
-tello.connect() # Connects the tello
-tello.takeoff() # Sends the command to the tello drone to takeoff
-tello.move_up(80) # Sends the command to move the tello upwards 80cm
+drone.connect() # Connects the tello
+drone.takeoff() # Sends the command to the tello drone to takeoff
+drone.move_up(80) # Sends the command to move the tello upwards 80cm
 
-tello.move_down(50)  # Sends the command to move the tello 50cm downwards
+drone.move_down(50)  # Sends the command to move the tello 50cm downwards
 
-tello.move_left(50)  # Sends the command to move the tello  50cm to the left
+drone.move_left(50)  # Sends the command to move the tello  50cm to the left
 
-tello.move_right(50) # Sends the command to move the tello  50cm to the right
+drone.move_right(50) # Sends the command to move the tello  50cm to the right
 
-tello.land() # sends a landing command to the tello
+drone.land() # sends a landing command to the tello
 
-tello.disconnect() # Disconnects the tello to the code
+drone.disconnect() # Disconnects the tello to the code
 
 time.sleep(2) #given in seconds 
 
